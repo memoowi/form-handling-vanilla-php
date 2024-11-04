6785fc9ffc8644b926c1527ef0676aed1b977330
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2024-11-04 14:51:23 +0700

    delete fix

--- delete_student.php
+++ delete_student.php
@@ -5,12 +5,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         if (isset($_POST["id"])) {
             $id = $_POST["id"];
+            $query = "SELECT photo FROM students WHERE id = '$id'";
+            $photo = mysqli_fetch_assoc($conn->query($query))["photo"];
+            if (file_exists($photo)) {
+                unlink($photo);
+            }
             $sql = "DELETE FROM students WHERE id = '$id'";
             $result = $conn->query($sql);
-            $old_photo = $result->fetch_assoc()["photo"];
-            if (file_exists($old_photo)) {
-                unlink($old_photo);
-            }
             if ($result) {
                 header("Location: index.php");
             }
